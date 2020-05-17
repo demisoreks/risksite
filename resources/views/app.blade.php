a63d90d853179323b8eee05267e925962ba95519
Demilade Soremekun <demiladesoremekun@gmail.com>
2020-05-17 14:44:06 +0100

    Addition of chat box

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -384,6 +384,8 @@
                 </div>
             </div>
         </div>
+        
+        <script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'b4530073-d8b7-47c1-8f81-7afc22eede82', f: true }); done = true; } }; })();</script>
 
     </body>
 </html>
